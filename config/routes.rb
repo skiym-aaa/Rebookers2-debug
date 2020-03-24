b3e79c2e79c84ebde05e041c9c590761b11a2663
skiym-aaa <y.sakiyama3216@gmail.com>
2020-03-24 12:46:29 +0900

    [Add] Search

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about' => 'home#about'
+  get '/search', to: 'search#search'
 
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
